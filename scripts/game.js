98e139462be21d7d6c1db7c78208507463de2e75
Alexandre Lima <alexandreatlima@gmail.com>
2022-03-30 11:01:13 -0300

    some logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,22 @@
 class Game {
   constructor(pairs) {
     this.pairs = pairs;
+    this.arrayPositions = [];
+    this.turnPlaysCounter = 0;
+    this.clickedImage1 = "";
+    this.clickedImage2 = "";
   }
 
   startGame(htmlCollection) {
     const arrayPositions = this.pairPositions();
+    this.arrayPositions = arrayPositions;
 
-    for (let i = 0; i < htmlCollection.length; i++) {
-      htmlCollection[i].setAttribute(
-        "src",
-        `./assets/images/${arrayPositions[i]}.jpg`
-      );
-    }
+    // for (let i = 0; i < htmlCollection.length; i++) {
+    //   htmlCollection[i].setAttribute(
+    //     "src",
+    //     `./assets/images/${arrayPositions[i]}.jpg`
+    //   );
+    // }
   }
 
   pairPositions() {
@@ -25,5 +30,40 @@ class Game {
     return positions;
   }
 
-  pairCheck() {}
+  flipImg(htmlElement, index) {
+    htmlElement.setAttribute(
+      "src",
+      `./assets/images/${this.arrayPositions[index]}.jpg`
+    );
+    this.turnPlaysCounter++;
+
+    if (this.turnPlaysCounter === 1) {
+      this.clickedImage1 = htmlElement;
+    } else if (this.turnPlaysCounter === 2) {
+      this.clickedImage2 = htmlElement;
+    }
+    this.pairCheck();
+  }
+
+  pairCheck() {
+    if (this.turnPlaysCounter !== 2) {
+      return;
+    }
+
+    if (
+      this.clickedImage1.getAttribute("src") !==
+      this.clickedImage2.getAttribute("src")
+    ) {
+      this.clickedImage1.setAttribute("src", "./assets/images/coverImg.jpg");
+      this.clickedImage2.setAttribute("src", "./assets/images/coverImg.jpg");
+    } else {
+      this.clickedImage1.removeEventListener();
+      this.clickedImage2.removeEventListener();
+    }
+  }
+  resetGame(htmlCollection) {
+    for (let i = 0; i < htmlCollection.length; i++) {
+      htmlCollection[i].setAttribute("src", "./assets/images/coverImg.jpg");
+    }
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 console.log("Hello World");
 const imgCollection = document.getElementsByClassName("game-img");
-const startBtn = document.getElementById("start-btn");
+const controlBtn = document.getElementById("control-btn");
 
 const game = new Game(5);
 
-startBtn.addEventListener("click", () => {
-  game.startGame(imgCollection);
+controlBtn.addEventListener("click", () => {
+  if (controlBtn.innerText === "Start") {
+    game.startGame(imgCollection);
+    controlBtn.innerText = "Reset";
+  } else if (controlBtn.innerText === "Reset") {
+    game.resetGame(imgCollection);
+    controlBtn.innerText = "Start";
+  }
 });
+
+// COMO COLOCAR UM EVENTLISTENER EM CADA ELEMENTO DE UMA HTML COLLECTION???? > FOR
+
+for (let i = 0; i < imgCollection.length; i++) {
+  imgCollection[i].addEventListener("click", () => {
+    game.flipImg(imgCollection[i], i);
+  });
+}
