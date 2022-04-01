4c322d55c5e7599fe293aae4c6381ad87869971a
Alexandre Lima <alexandreatlima@gmail.com>
2022-04-01 10:22:03 -0300

    bug fix btn start

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,30 @@ controlBtn.addEventListener("click", () => {
   if (controlBtn.innerText === "Start") {
     game.startGame();
     controlBtn.innerText = "Reset";
+    // Fazer um toggle e ajustar a classe do botão, pegando aquela classe do bootstrap
+    controlBtn.classList.toggle("btn-danger");
+    controlBtn.classList.toggle("btn-success");
+
+    for (let i = 0; i < imgCollection.length; i++) {
+      imgCollection[i].addEventListener("click", () => {
+        game.flipImg(imgCollection[i], i);
+      });
+    }
   } else if (controlBtn.innerText === "Reset") {
     game.resetGame();
     controlBtn.innerText = "Start";
+    controlBtn.classList.toggle("btn-danger");
+    controlBtn.classList.toggle("btn-success");
+
+    for (let i = 0; i < imgCollection.length; i++) {
+      const imgWithoutEventListner = document.createElement("img");
+      imgWithoutEventListner.setAttribute(
+        "src",
+        "./assets/images/coverImg.jpg"
+      );
+      imgWithoutEventListner.classList.add("game-img");
+
+      imgCollection[i].replaceWith(imgWithoutEventListner);
+    }
   }
 });
-
-// COMO COLOCAR UM EVENTLISTENER EM CADA ELEMENTO DE UMA HTML COLLECTION???? > FOR
-
-for (let i = 0; i < imgCollection.length; i++) {
-  imgCollection[i].addEventListener("click", () => {
-    game.flipImg(imgCollection[i], i);
-  });
-}
